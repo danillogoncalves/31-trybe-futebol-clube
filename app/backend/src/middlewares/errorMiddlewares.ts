89d547dd4b9b04dc6c54f3f1cec502c7df8650c0
Danillo Gonçalves Batista <danillogoncalves001@gmail.com>
2022-08-18 23:46:38 -0300

    feat: :white_check_mark: Middleware de Erros.
    Foi criado o errorMiddleware.

---
+++ app/backend/src/middlewares/errorMiddlewares.ts
@@ -0,0 +1,18 @@
+import { ErrorRequestHandler } from 'express';
+
+const errorMiddlewares: ErrorRequestHandler = (err, _req, res, _next) => {
+  const { name, message, details } = err;
+
+  switch (name) {
+    case 'ValidationError':
+      res.status(400).json({ message: details[0].message });
+      break;
+    case 'loginError':
+      res.status(401).json({ message });
+      break;
+    default:
+      res.status(500).json({ message });
+  }
+};
+
+export default errorMiddlewares;
